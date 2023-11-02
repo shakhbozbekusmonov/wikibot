df0dd3027d3ee97fada5a0b3e24b481687ece7e2
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-11-02 13:12:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+import wikipedia
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("BOT_TOKEN")
+wikipedia.set_lang("uz")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Wikipedia botiga hush kelibsiz, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def sendWiki(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        # await message.send_copy(chat_id=message.chat.id)
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Bu mavzuga oid maqola topilmadi!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
